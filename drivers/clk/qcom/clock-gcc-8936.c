149fab3703b750e36e716e76d265d86de267ba1b
PS-DEV <mahmod115.ma@gmail.com>
2017-08-21 04:37:37 +0300

    Update clock-gcc-8936.c

--- drivers/clk/qcom/clock-gcc-8936.c
+++ drivers/clk/qcom/clock-gcc-8936.c
@@ -851,7 +851,7 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-                VDD_DIG_FMAX_MAP4(LOW, 19200000, NOMINAL, 310000000, NOMINAL_PLUS, 400000000, HIGH, 650000000),
+                VDD_DIG_FMAX_MAP4(LOW, 19200000, NOMINAL, 310000000, NOMINAL_PLUS, 550000000, HIGH, 650000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
